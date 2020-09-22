e862b5d179c72f858eaf798fa12eb94ecd65d528
PitWrdLC <bogdanov2.pa@edu.spbstu.ru>
2020-09-22 14:33:13 +0300

    numberRevert accountInThreeYears travelMinutes

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -68,7 +68,16 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+   /* println( age)
+    if (age==1)||(age==1+) return println($age год)
+        else if (age>=4) return age "   "
+                else if (age>=20) return age"   "
+*/
+    if (age%10==1) return age
+    if (age%10>1)&&(age%10<5) return age
+    if (age%)
+} //= TODO()
 
 /**
  * Простая (2 балла)
